1f9c922ae493b6b4c2c4a4278a06c62a4dc465e7
relmossaoui <redouan.elmossaoui2@gmail.com>
2020-07-05 17:00:54 +0100

    reafcto : refacto http utility.

--- src/http.js
+++ src/http.js
@@ -2,44 +2,15 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pokeapi.co/api/v2/'
 
-export async function getPokemonPage({pageNumber = 0, pageSize = 20 }) {
-    
-    let url = `pokemon?limit=${pageSize}&offset=${pageSize * pageNumber}`
-
+/**
+ * fetch pokemons related data
+ * 
+ * @param { String } url            : resource url
+ * @param {*} defaultErrorMessage   : error message for error with status !== 404
+ */
+async function fetchData(url, defaultErrorMessage = "Data fetching failed") {
     try {
-        let { data } =  await axios.get(BASE_URL + url)  
-
-        return data
-    } catch(e) {
-        let { status } = e.response;
-
-        if (status === 404) 
-            throw 'unavailable resource'
-        
-        throw 'pokemons fetching failed'
-    }
-}
-
-export async function getPokemonTypes() {
-
-    try {
-        let { data } =  await axios.get(BASE_URL + 'type')  
-
-        return data
-    } catch(e) {
-        let { status } = e.response;
-
-        if (status === 404) 
-            throw 'unavailable resource'
-        
-        throw 'pokemon types fetching failed'
-    }  
-
-}
-
-export async function getPokemonsByType(type) {
-    try {
-        let { data } =  await axios.get(BASE_URL + 'type/' + type)   
+        let { data } =  await axios.get(BASE_URL + url)      
 
         return data
 
@@ -49,22 +20,14 @@ export async function getPokemonsByType(type) {
         if (status === 404) 
             throw 'unavailable resource'
         
-        throw 'pokemons fetching failed'
-    }
+        throw defaultErrorMessage
+    } 
 }
 
-export async function getPokemon(name) {
-    try {
-        let { data } =  await axios.get(BASE_URL + 'pokemon/' + name)      
+export const getPokemonPage     = async ({pageNumber = 0, pageSize = 20 }) => await fetchData(`pokemon?limit=${pageSize}&offset=${pageSize * pageNumber}`, 'Page fetching failed')
 
-        return data
+export const getPokemonTypes    = async () => await fetchData('type', 'pokemon types fetching failed')
 
-    } catch(e) {
-        let { status } = e.response
+export const getPokemonsByType  = async (type) => await fetchData('type/' + type, 'Pokemons fetching failed')
 
-        if (status === 404) 
-            throw 'unavailable resource'
-        
-        throw 'pokemon fetching failed'
-    }  
-}
+export const getPokemon         = async (name) => await fetchData('pokemon/' + name, 'Pokemon fetching failed') 
