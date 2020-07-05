a9e21ae6daf70a4d7c2a76e86cfbb563cec954fb
relmossaoui <redouan.elmossaoui2@gmail.com>
2020-07-05 16:15:30 +0100

    feat : add pokemon types selector.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import {
-    Link 
-} from 'react-router-dom'
+    Link,
+    useHistory
+} from 'react-router-dom';
+
+import { getPokemonTypes } from '../../http'
 
 import './Header.css';
 
 export default function(props) {
+    let [ types, setTypes ] = React.useState([])
+    let [ selectedType, setSelectedType ] = React.useState('')
 
+    const history = useHistory();
+
+    React.useEffect(() => {
+        const getTypes = async () => {
+            try {
+                let { results } = await getPokemonTypes()
+
+                setTypes(results)
+            } catch(e) {
+
+            }
+        }
+
+        getTypes()
+    }, [ selectedType ])
+
+    function onSelectType(e) {
+        setSelectedType(e.target.value)
+        history.push("/type/" + e.target.value);
+    }
 
     return (
         <ul className="header c-flex c-flex-sp-between c-flex-center-items">
             <li className="c-flex align-items"> <Link to="/"> Home </Link> </li>          
+            <li className=""> 
+                <select value={selectedType} onChange={onSelectType}>
+                    <option value={''} disabled> Select Type </option>
+                    { types.map(({ name }) => <option key={name} value={name}> {name} </option>)}
+                </select>
+            </li>
         </ul>
     )
 }
--- src/http.js
+++ src/http.js
@@ -7,7 +7,7 @@ export async function getPokemonPage({pageNumber = 0, pageSize = 20 }) {
     let url = `pokemon?limit=${pageSize}&offset=${pageSize * pageNumber}`
 
     try {
-        let { data, status, statusText } =  await axios.get(BASE_URL + url)  
+        let { data } =  await axios.get(BASE_URL + url)  
 
         return data
     } catch(e) {
@@ -18,4 +18,21 @@ export async function getPokemonPage({pageNumber = 0, pageSize = 20 }) {
         
         throw 'pokemons fetching failed'
     }
+}
+
+export async function getPokemonTypes() {
+
+    try {
+        let { data } =  await axios.get(BASE_URL + 'type')  
+
+        return data
+    } catch(e) {
+        let { status } = e.response;
+
+        if (status === 404) 
+            throw 'unavailable resource'
+        
+        throw 'pokemon types fetching failed'
+    }  
+
 }
